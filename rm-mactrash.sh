fc40f43c876e906639eb72e2e823457fea7094ea
leandropifano <81452995+leandropifano@users.noreply.github.com>
2021-04-10 00:43:25 -0300

    Update rm-mactrash.sh

--- rm-mactrash.sh
+++ rm-mactrash.sh
@@ -1,12 +1,15 @@
 ####SCRIPT QUE BORRA LOS ARCHIVOS BASURA QUE GENERA MAC OS######
 ####------------------------------------------------------------------------------------------
 #!/bin/bash
+#Muestro la ruta en la que voy a borrar
 pwd
 echo "^^-------------------------------------------------------------------------------------^^"
+#Pregunto
 read -p "¿Borrar RECURSIVAMENTE TODOS los archivos '.DS_STORE' y '._nombre.extension'? (y/n)" resp
+#Opciones
 case "$resp" in
         y|Y|s|S )       find -name '.DS*' -exec rm -fvI {} \;
-                find -name '._*' -exec rm -fvI {} \;
+                        find -name '._*' -exec rm -fvI {} \;
                 ;;
         n|N )   echo "ABORTADO";;
         * )     echo "Opcion incorrecta!";;
